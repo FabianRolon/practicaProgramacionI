05ae17944cf328fe3f2b632f1c0a19daf4ca6af5
alumno <alumno@LAB00PC00>
2019-04-09 20:35:10 -0300

    creo funcion para validar si tengo un nombre valido

---
+++ clase_06_strings/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAMANO_ARRAY 7
+
+int sValidName(char* sName, int sLength);
+
+int main()
+{
+   /**
+    char nombre[257]={'H','O','L','A','\0'}; //se declara la cantidad que quiero usar +1, ese +1 es
+    char nombre2[257]="HOLA";//esto equivale a lo de arriba                        //para el "\0" que determinja el final de los datos usar
+
+    //sizeof(nombre); //dice que tamaño tiene asignado en memoria ese array
+    strncpy(nombre, nombre2,sizeof(nombre)); //se le pasa destino, origen y tamaño (nunca me tengo que psar del tamaño del destino)
+    printf("\n%s",nombre);                                       //para que no provoque un overflow
+    fgets(nombre, sizeof(nombre), stdin);
+    printf("\n%s",nombre);
+    strlen(); //da la cantidad de caracteres de un string
+    strcmp(); //me dice cual es mas grande de los string que quiera
+
+    //tarea: aplicacion que pida el nombre de usuario, guarda en buffer,
+    //vamos a verificar la cant de char,
+    //verificar que no tenga caracteres que no quiera,
+    //si es un nombre guardar en la variable de destino y mostrar en pantalla.
+    **/
+
+    char nombre[TAMANO_ARRAY];
+    char buffer[TAMANO_ARRAY];
+    int lengthBuffer;
+
+    printf("Ingrese su nombre querido usuario: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    lengthBuffer = strlen(buffer);
+
+    if(sValidName(buffer, lengthBuffer))
+    {
+        strncpy(nombre, buffer,sizeof(nombre));
+        printf("\n%s",nombre);
+    }
+    else
+    {
+        printf("\nERROR, INGRESE SOLO LETRAS");
+    }
+    return 0;
+}
+
+
+
+
+
+
+int sValidName(char* sName, int sLength)
+{
+   int i;
+   int retorno = 1;
+
+    for(i=0; i < sLength; i++)
+    {
+        if((sName[i] != ' ') && (sName[i] < 'a' || sName[i] > 'z') && (sName[i] < 'A' || sName[i] > 'Z'))
+        {
+            retorno = 0;
+        }
+    }
+    return retorno;
+}
